557001b82cf09a94fec062d4e82c9b549d29020a
Cédric Bousmanne <cedric@bousmanne.com>
2020-04-06 09:01:14 +0200

    Add monitor switch script

---
+++ .config/scripts/monitor.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+intern=eDP-1
+extern=DP-2
+internalSettings="xrandr --output $intern --primary --mode 3840x2160 --scale 1x1"
+
+if xrandr | grep "$extern disconnected"; then
+  $internalSettings --output $extern --off
+  echo "-> Laptop Mode"
+
+# elif xrandr | grep "$extern connected 4160x2640+0+0"; then
+#   $internalSettings --fb 4160x2640
+#   xrandr --output $extern --scale 2x2 --mode 3840x2160 
+#   echo "-> Desktop Mode - Asus 4K"
+
+elif xrandr | grep "$extern connected 3840x2160+0+0"; then
+  # $internalSettings --fb 3840x2160
+  $internalSettings --fb 5760x2160
+  xrandr --output $extern --scale 2x2 --mode 1920x1080
+  echo "-> Desktop Mode - Samsung 1920x1080"
+
+else
+  $internalSettings --fb 3840x2160
+  xrandr --output $extern --scale 2x2 --mode 1920x1080
+  echo "-> Desktop Mode 1920x1080"
+
+fi
