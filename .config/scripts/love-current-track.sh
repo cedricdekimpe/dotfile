5e0da05845229859f0554878598001beaa249f6c
Cédric Bousmanne <cedric@bousmanne.com>
2020-04-19 00:47:50 +0200

    Automatically retrieve new spotify token and retry request

--- .config/scripts/love-current-track.sh
+++ .config/scripts/love-current-track.sh
@@ -1,20 +1,34 @@
 #/bin/bash
 
 # Need a token with `user-library-modify` scope
+# How to get a valid token from the CLI
+#
+# npm install -g spotify-auth-cli
+# spotify-token --scope=user-library-modify
 
-source /etc/environment
+touch ~/.spotify_token
+source ~/.spotify_token
 
 current_track_id=$(mpc --format "%file%" | head -n 1 | awk -F ":" '{ print $3 }')
 current_track_name=$(mpc --format "%title%" | head -n 1)
 
-[ -z "$SPOTIFY_USER_LIBRARY_MODIFY_TOKEN" ] && notify-send "Could not save song. Please specify a SPOTIFY_USER_LIBRARY_MODIFY_TOKEN in /etc/environment"
+result=$(curl -X "PUT" "https://api.spotify.com/v1/me/tracks?ids=$current_track_id" -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer $SPOTIFY_USER_LIBRARY_MODIFY_TOKEN" | jq)
 
-status=$(curl -X "PUT" "https://api.spotify.com/v1/me/tracks?ids=$current_track_id" -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer $SPOTIFY_USER_LIBRARY_MODIFY_TOKEN" -w '%{http_code}')
 
-if [ $status -eq 200 ]; then
+error=$(echo $result | jq .error.message)
+status=$(echo $result | jq .error.status)
+
+echo $error
+if [ -z "$error" ]; then
   notify-send "❤️  Loved $current_track_name on Spotify 🎵"
   break
 else
-  notify-send "💔 Couldn't love $current_track_name, Got $status" | tr '\n' ' '
-  break
+  notify-send "💔 Couldn't love $current_track_name, $error [$status]" | tr '\n' ' '
+  if [ $status -eq 401 ]; then
+    token=$(spotify-token --scope=user-library-modify | gawk '{split($0,a); print a[4]}' | head -2 | tail -1)
+    echo "SPOTIFY_USER_LIBRARY_MODIFY_TOKEN=$token" > ~/.spotify_token
+    notify-send "New token received, retrying"
+    $("$0")
+  fi
 fi
+
