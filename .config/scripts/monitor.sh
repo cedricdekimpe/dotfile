b6a251c31af0886d80e935c4d3ddefa8045dfef7
Cédric Bousmanne <cedric@bousmanne.com>
2022-03-31 11:31:25 +0200

    savage update

--- .config/scripts/monitor.sh
+++ .config/scripts/monitor.sh
@@ -1,7 +1,35 @@
 #!/bin/bash
-intern=eDP-1
-extern=DP-2
-internalSettings="xrandr --output $intern --primary --mode 3840x2160 --scale 1x1"
+
+
+xrandr --output eDP1 --primary --mode 3200x1800 --scale 0.6x0.6
+# xrandr --output DP1 --mode 2560x1080 --scale 1x1 # AppTweak LG
+# xrandr --output DP1 --mode 1920x1080 --scale 1x1 # Home
+
+
+exit
+
+intern=eDP1
+extern=DP1
+# extern=DP3
+internalSettings="xrandr --output $intern --primary --mode 3840x2160 --scale 0.6x0.6"
+
+screenIDRegex="[[:alpha:]]{2}[[:digit:]]{1}"
+screenResolutionRegex="[[:digit:]]{3,4}x[[:digit:]]{3,4}"
+connectedScreensRegex="$screenIDRegex connected $screenResolutionRegex"
+
+connectedScreenID=$(xrandr | egrep "${connectedScreensRegex}" -o | egrep "${screenIDRegex}" -o)
+connectedScreenResolution=$(xrandr | egrep "${connectedScreensRegex}" -o | egrep "${screenResolutionRegex}" -o)
+
+echo $connectedScreenID
+echo $connectedScreenResolution
+
+xrandr --output $connectedScreenID --scale 1x1 --mode $connectedScreenResolution
+
+
+
+
+
+exit
 
 if xrandr | grep "$extern disconnected"; then
   $internalSettings --output $extern --off
@@ -21,7 +49,8 @@ if xrandr | grep "$extern disconnected"; then
 else
   # $internalSettings --fb 5760x2160
   $internalSettings --fb 7680x2160 --pos 3840x0
-  xrandr --output $extern --scale 2x2 --mode 1920x1080
+  xrandr --output $extern --scale 1x1 --mode 1920x1080
+  # xrandr --output $extern --scale 1x1 --mode 2560x1080 # AppTweak LG
   echo "-> Desktop Mode 1920x1080"
 
 fi
